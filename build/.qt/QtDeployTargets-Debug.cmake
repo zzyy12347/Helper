73ea3911dd6d422da111d47d972c065e93420646
homya <zzyy12347@163.com>
2026-07-09 07:01:14 +0800

    1

---
+++ build/.qt/QtDeployTargets-Debug.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ItemShopFinder_FILE D:/AIWork/Helper/build/Debug/ItemShopFinder.exe)
+set(__QT_DEPLOY_TARGET_ItemShopFinder_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ItemShopFinder_RUNTIME_DLLS C:/Qt/6.10.0/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Cored.dll)
---
+++ build/.qt/QtDeployTargets-MinSizeRel.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ItemShopFinder_FILE D:/AIWork/Helper/build/MinSizeRel/ItemShopFinder.exe)
+set(__QT_DEPLOY_TARGET_ItemShopFinder_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ItemShopFinder_RUNTIME_DLLS C:/Qt/6.10.0/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Core.dll)
